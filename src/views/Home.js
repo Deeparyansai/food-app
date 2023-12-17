7c6cebfa80e8605faf1b17669c4e7ab7bf52c321
Deeparyansai <Deeparyansai@deep.com>
2023-12-17 13:57:00 +0530

    created navbar

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navibar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+    <div>Home</div>
+    <div><Navibar /></div>
+    <div><Footer /></div>
+    </>
+  )
+}
